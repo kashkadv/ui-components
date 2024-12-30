cce34a51a4c7298953762e553d81823a2e11e940
Dmytro Kashka <dmytro.kashka@beetroot.se>
2024-12-30 12:09:12 +0200

    Gallery update

--- Components/Buttons/CartButton.js
+++ Components/Buttons/CartButton.js
@@ -3,10 +3,10 @@ import UseIcon from "../UI/UseIcon"
 
 function CartButton({ label = 'Add to cart' }) { // TODO remove label and direction
   return (
-    <Button type='primary' className={`group relative [&>*]:transition-all [&>*]:duration-500 gap-4 px-4 py-2 h-8 items-end flex text-greyDark z-0`}>
+    <Button type='primary' className={`group relative [&>*]:transition-all [&>*]:duration-500 gap-4 px-4 py-2 h-8 items-center flex text-greyDark z-0`}>
       <div className="leading-none tracking-wide font-bold flex items-center group relative group-hover:text-white z-10">{label}</div>
       <UseIcon w='5' id="cart" className={`mb-[2px] text-green group-hover:text-white ltr:-ml-1 ltr:mr-1 group-hover:ltr:ml-1 group-hover:ltr:mr-0 rtl:ml-0 rtl:-mr-1 group-hover:rtl:-ml-1 group-hover:rtl:mr-1 rtl:transform rtl:rotate-y-180 z-10`} />
-      <div className={`h-[2px] w-1/2 bg-green absolute -bottom-1 transform group-hover:w-full group-hover:h-11 -z-0 ltr:right-5 group-hover:ltr:right-0 rtl:left-4 group-hover:rtl:left-0`}></div> 
+      <div className={`h-[2px] w-1/2 bg-green absolute -bottom-1 transform group-hover:w-full group-hover:h-10 -z-0 ltr:right-5 group-hover:ltr:right-0 rtl:left-4 group-hover:rtl:left-0`}></div> 
     </Button>
   )
 }
--- Components/Product/ProductGallery.js
+++ Components/Product/ProductGallery.js
@@ -1,12 +1,19 @@
 import { useRef, useState, useEffect } from "react";
 import Image from "next/image";
 import { useProductPage } from "../Pages/ProductPage";
+import UseIcon from "../UI/UseIcon";
 
 function ProductGallery() {
   const { images } = useProductPage()
 
   const containerRef = useRef(null); // Ref for scroll container
-  const [activeIndex, setActiveIndex] = useState(0); // Track active image index  
+  const [activeIndex, setActiveIndex] = useState(0); // Track active image index
+  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handlePopupState = () => {
+    setIsOpen(!isOpen)
+  };
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -44,38 +51,79 @@ function ProductGallery() {
 
   return (
     <div className="relative flex items-center justify-center w-full bg-white/90  h-full shadow-xl">
-
-      <div ref={containerRef} className="h-[calc(100%-200px)] w-[calc(100%-200px)] overflow-y-scroll no-scrollbar relative">
+      <div ref={containerRef} onClick={handlePopupState} className="cursor-pointer h-[calc(100%-200px)] w-[calc(100%-200px)] overflow-y-scroll no-scrollbar relative">
         {images.map((src, index) => (
-          <div
-            key={index}
-            data-index={index}
-            className="image-item aspect-[3/4] relative"
-          >
+          <div key={index} data-index={index} className="image-item aspect-[3/4] relative">
             <Image src={src} alt={`Image ${index + 1}`} fill />
           </div>
         ))}
+
       </div>
 
       <div className="absolute left-1/2 bottom-0 flex items-center justify-between -translate-x-1/2 z-10 w-full h-[100px] px-[100px]">
+
         <div className="flex items-center gap-2 font-semibold text-grey">
-          <div>{activeIndex + 1}</div>
-          <div className="w-[2px] bg-grey h-[20px] rotate-12"></div>
-          <div>{images.length}</div>
+          <div className="text-greyDark">{activeIndex + 1}</div>
+          <div className="w-[1px] bg-greyLight h-[20px] "></div>
+          <div className="text-greyLight">{images.length}</div>
         </div>
-        <div className="flex gap-3">
+
+        <PopupTrigger handlePopupState={handlePopupState} />
+        
+        <div className="flex items-center gap-2">
           {images.map((_, index) => (
             <button
-              key={index}
-              onClick={() => scrollToImage(index)}
-              className={`!w-3 !h-3 flex rounded-full transition bg-grey border-grey ${
-                activeIndex === index ? "bg-grey" : "opacity-10"
-              }`}
+            key={index}
+            onClick={() => scrollToImage(index)}
+            className={`w-2 h-2 flex transition-all rounded-full bg-grey border-grey ${
+              activeIndex === index ? "bg-greyDark !w-3 !h-3" : "opacity-10"
+            }`}
             ></button>
           ))}
         </div>
       </div>
+
+      <GalleryPopup isOpen={isOpen} handlePopupState={handlePopupState} />
     </div>
   )
 }
+
+function PopupTrigger({ handlePopupState }) {
+  return (
+    <button className=" z-10 bottom-0 right-0" onClick={handlePopupState}>
+      <UseIcon id="search-plus" w="6" className="text-greyDark transition-all hover:text-greyLight" />
+    </button>
+  )
+}
+
+function GalleryPopup({isOpen, handlePopupState}) {
+  const { images } = useProductPage()  
+
+  if (!isOpen) return 
+
+  return (
+    <div
+      className="fixed w-full left-0 top-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-12"
+      onClick={handlePopupState}
+    >
+      <div
+        className="overflow-y-auto max-h-[calc(100dvh-96px)] w-full no-scrollbar shadow-xl"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          className="absolute top-16 right-16 text-3xl font-bold text-black bg-transparent border-none cursor-pointer"
+          onClick={handlePopupState}
+        >
+          <UseIcon id="x" w="8" className="text-greyDark transition-all hover:text-grey" />
+        </button>
+        <div className="flex flex-col bg-red-100 w-full">
+          {images.map((src, index) => (
+            <img key={index} src={src} alt={`Image ${index}`} className="w-full h-auto object-cover" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default ProductGallery
