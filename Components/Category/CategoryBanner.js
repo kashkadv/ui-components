70b070aa458d4376d9abb222b7737e3c28aca779
Dmytro Kashka <dmytro.kashka@beetroot.se>
2025-01-03 14:22:14 +0200

    category page

--- Components/Category/CategoryBanner.js
+++ Components/Category/CategoryBanner.js
@@ -2,7 +2,12 @@ function CategoryBanner({ category }) {
   const { name, description } = category
 
   return (
-    <div className="w-full p-12 bg-red-100">{name}</div>
+    <div className="w-full p-12 grid grid-cols-2">
+      <div className="space-y-6">
+        <h1 className="text-h1 font-black">{name}</h1>
+        <div className="leading-loose">{description}</div>
+      </div>
+    </div>
   )
 }
 export default CategoryBanner
